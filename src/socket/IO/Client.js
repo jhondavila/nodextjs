a5192a9569764257863eb4b14b0b806f7cab913e
Jhon Erlind Davila Mendez <jhondavilamendez@gmail.com>
2018-02-22 11:38:14 -0500

    fix socket stream in socket io

--- src/socket/IO/Client.js
+++ src/socket/IO/Client.js
@@ -1,7 +1,6 @@
 /**
  * 
  */
-
 Nodext.define("Nodext.socket.IO.Client", {
     extend: "Nodext.Base",
     $configPrefixed: false,
@@ -16,6 +15,8 @@ Nodext.define("Nodext.socket.IO.Client", {
     },
     cnx: null,
     urlSpace: null,
+    io: require('socket.io-client'),
+    ss: require('socket.io-stream'),
     constructor: function (config) {
         Nodext.apply(this, config || {});
         this.initConfig(config);
@@ -30,7 +31,7 @@ Nodext.define("Nodext.socket.IO.Client", {
             }
             params = listParams.join("&");
         }
-        this.cnx = io.connect(this.urlSpace, {
+        this.socket = this.cnx = this.io.connect(this.urlSpace, {
             multiplex: this.multiplex,
             autoConnect: false,
             query: params
@@ -72,19 +73,19 @@ Nodext.define("Nodext.socket.IO.Client", {
     }
 });
 
-
-var io = require('socket.io-client');
-var ss = require('socket.io-stream');
-
-
 (function () {
-    var IOproto = io.prototype.constructor;
+    var IOproto = Nodext.socket.IO.Client.prototype.io.prototype.constructor;
     var Socket = IOproto.Socket.prototype;
     var Manager = IOproto.Manager.prototype;
+    var Emitter = require('component-emitter');
+    var emit = Emitter.prototype.emit;
+
     Socket.onevent = function (t) {
         var e = t.data || [];
         null != t.id && e.push(this.ack(t.id));
         if (this.connected) {
+
+            emit.apply(this, e);
             if (this._extSocket) {
                 var date = new Date();
                 e.splice(1, 0, this._extSocket);
@@ -132,39 +133,89 @@ var ss = require('socket.io-stream');
         }
     };
 
+    // Manager.reconnect = function () {
+    //     if (this.reconnecting || this.skipReconnect)
+    //         return this;
+    //     var t = this;
+    //     if (this.backoff.attempts >= this._reconnectionAttempts)
+    //         this.backoff.reset(), this.emitAll("reconnect_failed"), this.reconnecting = !1;
+    //     else {
+    //         var e = this.backoff.duration();
+    //         this.reconnecting = !0;
+    //         var r = setTimeout(function () {
+    //             if (!t.skipReconnect) {
+    //                 t.emitAll("reconnect_attempt", t.backoff.attempts);
+    //                 t.emitAll("reconnecting", t.backoff.attempts);
+    //                 if (Ext.isObject(t.nsps)) {
+    //                     var x, i;
+    //                     for (x in t.nsps) {
+    //                         i = t.nsps[x];
+    //                         if (i._extSocket) {
+    //                             i._extSocket.fireEvent("reconnecting", i._extSocket, t.backoff.attempts);
+    //                         }
+    //                     }
+    //                 }
+    //                 t.skipReconnect || t.open(function (e) {
+    //                     e ? (t.reconnecting = !1, t.reconnect(), t.emitAll("reconnect_error", e.data)) : t.onreconnect()
+    //                 });
+    //             }
+    //         }, e);
+    //         this.subs.push({
+    //             destroy: function () {
+    //                 clearTimeout(r);
+    //             }
+    //         });
+    //     }
+    // };
+
     Manager.reconnect = function () {
-        if (this.reconnecting || this.skipReconnect)
-            return this;
-        var t = this;
-        if (this.backoff.attempts >= this._reconnectionAttempts)
-            this.backoff.reset(), this.emitAll("reconnect_failed"), this.reconnecting = !1;
-        else {
-            var e = this.backoff.duration();
-            this.reconnecting = !0;
-            var r = setTimeout(function () {
-                if (!t.skipReconnect) {
-                    t.emitAll("reconnect_attempt", t.backoff.attempts);
-                    t.emitAll("reconnecting", t.backoff.attempts);
-                    if (Ext.isObject(t.nsps)) {
-                        var x, i;
-                        for (x in t.nsps) {
-                            i = t.nsps[x];
-                            if (i._extSocket) {
-                                i._extSocket.fireEvent("reconnecting", i._extSocket, t.backoff.attempts);
-                            }
+        if (this.reconnecting || this.skipReconnect) return this;
+
+        var self = this;
+
+        if (this.backoff.attempts >= this._reconnectionAttempts) {
+            this.backoff.reset();
+            this.emitAll('reconnect_failed');
+            this.reconnecting = false;
+        } else {
+            var delay = this.backoff.duration();
+
+            this.reconnecting = true;
+            var timer = setTimeout(function () {
+                if (self.skipReconnect) return;
+
+                self.emitAll('reconnect_attempt', self.backoff.attempts);
+                self.emitAll('reconnecting', self.backoff.attempts);
+
+                if (Ext.isObject(self.nsps)) {
+                    var x, i;
+                    for (x in self.nsps) {
+                        i = self.nsps[x];
+                        if (i._extSocket) {
+                            i._extSocket.fireEvent("reconnecting", i._extSocket, self.backoff.attempts);
                         }
                     }
-                    t.skipReconnect || t.open(function (e) {
-                        e ? (t.reconnecting = !1, t.reconnect(), t.emitAll("reconnect_error", e.data)) : t.onreconnect()
-                    });
                 }
-            }, e);
+
+                // check again for the case socket closed in above events
+                if (self.skipReconnect) return;
+
+                self.open(function (err) {
+                    if (err) {
+                        self.reconnecting = false;
+                        self.reconnect();
+                        self.emitAll('reconnect_error', err.data);
+                    } else {
+                        self.onreconnect();
+                    }
+                });
+            }, delay);
+
             this.subs.push({
                 destroy: function () {
-                    clearTimeout(r);
+                    clearTimeout(timer);
                 }
             });
         }
     };
-
 })();
